3dae4d16d502a3ce2ecfbdc9b3bc8685e3a0f1c0
rohitpardhi1998 <rohit pardhi>
2018-02-21 20:39:19 +0530

    Initial commit

---
+++ App.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import App from './App';
+
+import renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+export default class HelloWorldApp extends Component {
+  render() {
+    return (
+      <Text>Hello world!</Text>
+    );
+  }
+}
+
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => HelloWorldApp);
+
